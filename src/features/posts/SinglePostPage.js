49e7c387fd9e4950a02c2faae503d7b749eb25b8
zavanton123 <zavanton123@gmail.com>
2021-03-11 19:58:25 +0300

    time age filtering reactions

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -3,6 +3,7 @@ import {useSelector} from "react-redux";
 import {Link} from "react-router-dom";
 import {PostAuthor} from "./PostAuthor";
 import {TimeAgo} from "./TimeAgo";
+import {ReactionButtons} from "./ReactionButtons";
 
 
 export const SinglePostPage = ({match}) => {
@@ -26,6 +27,7 @@ export const SinglePostPage = ({match}) => {
         <PostAuthor userId={post.user}/>
         <TimeAgo timestamp={post.date} />
         <p className="post-content">{post.content}</p>
+        <ReactionButtons post={post} />
         <Link to={`/editPost/${post.id}`}>
           Edit Post
         </Link>
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,9 +1,24 @@
 import {createSlice, nanoid} from "@reduxjs/toolkit";
+import {sub} from 'date-fns';
 
 
 const initialState = [
-  {id: '1', date: '2021-01-05T14:48:00.000Z', title: 'First Post', content: 'Hello', user: '0'},
-  {id: '2', date: '2021-02-05T14:48:00.000Z', title: 'Second Post', content: 'More text', user: '1'}
+  {
+    id: '1',
+    date: sub(new Date(), {minutes: 10}).toISOString(),
+    title: 'First Post',
+    content: 'Hello',
+    user: '0',
+    reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
+  },
+  {
+    id: '2',
+    date: sub(new Date(), {minutes: 5}).toISOString(),
+    title: 'Second Post',
+    content: 'More text',
+    user: '1',
+    reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
+  }
 ]
 
 
