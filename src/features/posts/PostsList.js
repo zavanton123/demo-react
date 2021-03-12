a61b00cf10119be9ddc8d016af462eb98f9538fc
zavanton123 <zavanton123@gmail.com>
2021-03-12 10:52:09 +0300

    ok more

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import {useSelector} from "react-redux";
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
 import {Link} from "react-router-dom";
 import {PostAuthor} from "./PostAuthor";
 import {TimeAgo} from "./TimeAgo";
 import {ReactionButtons} from "./ReactionButtons";
-import {selectAllPosts} from "./postsSlice";
+import {fetchPosts, selectAllPosts} from "./postsSlice";
 
 
 export const PostsList = () => {
+  const dispatch = useDispatch();
   const posts = useSelector(selectAllPosts);
+
+  const postStatus = useSelector(state => state.posts.status);
+
+  useEffect(() => {
+    if (postStatus === 'idle') {
+      dispatch(fetchPosts());
+    }
+  }, [postStatus, dispatch]);
 
   const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
 
@@ -16,10 +25,10 @@ export const PostsList = () => {
     return (
       <article className="post-excerpt" key={post.id}>
         <h3>{post.title}</h3>
-        <PostAuthor userId={post.user} />
-        <TimeAgo timestamp={post.date} />
+        <PostAuthor userId={post.user}/>
+        <TimeAgo timestamp={post.date}/>
         <p className="post-content">{post.content.substring(0, 100)}</p>
-        <ReactionButtons post={post} />
+        <ReactionButtons post={post}/>
         <Link
           to={`/posts/${post.id}`}
           className="button muted-button"
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,25 +1,17 @@
-import {createSlice, nanoid} from "@reduxjs/toolkit";
-import {sub} from 'date-fns';
+import {createAsyncThunk, createSlice, nanoid} from "@reduxjs/toolkit";
+import {client} from "../../api/client";
 
 
-const initialState = [
-  {
-    id: '1',
-    date: sub(new Date(), {minutes: 10}).toISOString(),
-    title: 'First Post',
-    content: 'Hello',
-    user: '0',
-    reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
-  },
-  {
-    id: '2',
-    date: sub(new Date(), {minutes: 5}).toISOString(),
-    title: 'Second Post',
-    content: 'More text',
-    user: '1',
-    reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
-  }
-]
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null
+}
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await client.get('/fakeApi/posts');
+  return response.posts;
+})
 
 
 const postsSlice = createSlice({
@@ -28,7 +20,7 @@ const postsSlice = createSlice({
   reducers: {
     postAdded: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.posts.push(action.payload);
       },
       prepare(title, content, userId) {
         return {
@@ -44,7 +36,7 @@ const postsSlice = createSlice({
     },
     postUpdated(state, action) {
       const {id, title, content} = action.payload;
-      const existingPost = state.find(post => post.id === id);
+      const existingPost = state.posts.find(post => post.id === id);
       if (existingPost) {
         existingPost.title = title;
         existingPost.content = content;
@@ -52,10 +44,23 @@ const postsSlice = createSlice({
     },
     reactionAdded(state, action) {
       const {postId, reaction} = action.payload;
-      const existingPost = state.find(post => post.id === postId);
+      const existingPost = state.posts.find(post => post.id === postId);
       if (existingPost) {
         existingPost.reactions[reaction]++;
       }
+    }
+  },
+  extraReducers: {
+    [fetchPosts.pending]: (state, action) => {
+      state.status = 'loading';
+    },
+    [fetchPosts.fulfilled]: (state, action) => {
+      state.status = 'succeeded';
+      state.posts = state.posts.concat(action.payload);
+    },
+    [fetchPosts.rejected]: (state, action) => {
+      state.status = 'failed';
+      state.error = action.error.message
     }
   }
 })
@@ -64,6 +69,6 @@ export const {postAdded, postUpdated, reactionAdded} = postsSlice.actions;
 
 export default postsSlice.reducer;
 
-export const selectAllPosts = state => state.posts;
+export const selectAllPosts = state => state.posts.posts;
 
-export const selectPostById = (state, postId) => state.posts.find(post => post.id === postId);
+export const selectPostById = (state, postId) => state.posts.posts.find(post => post.id === postId);
