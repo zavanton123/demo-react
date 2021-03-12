ab3a712595fc8c5b70558f4947eafbcf77b989b0
zavanton123 <zavanton123@gmail.com>
2021-03-12 10:56:10 +0300

    ok more more

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -12,6 +12,7 @@ export const PostsList = () => {
   const posts = useSelector(selectAllPosts);
 
   const postStatus = useSelector(state => state.posts.status);
+  const error = useSelector(state => state.posts.error);
 
   useEffect(() => {
     if (postStatus === 'idle') {
@@ -19,28 +20,58 @@ export const PostsList = () => {
     }
   }, [postStatus, dispatch]);
 
-  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
+  let content;
 
-  const renderedPosts = orderedPosts.map(post => {
-    return (
-      <article className="post-excerpt" key={post.id}>
-        <h3>{post.title}</h3>
-        <PostAuthor userId={post.user}/>
-        <TimeAgo timestamp={post.date}/>
-        <p className="post-content">{post.content.substring(0, 100)}</p>
-        <ReactionButtons post={post}/>
-        <Link
-          to={`/posts/${post.id}`}
-          className="button muted-button"
-        >View Post</Link>
-      </article>
-    );
-  });
+  if (postStatus === 'loading') {
+    content = <div className="loader">Loading...</div>
+  } else if (postStatus === 'succeeded') {
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map(post => {
+      return (
+        <article className="post-excerpt" key={post.id}>
+          <h3>{post.title}</h3>
+          <PostAuthor userId={post.user}/>
+          <TimeAgo timestamp={post.date}/>
+          <p className="post-content">{post.content.substring(0, 100)}</p>
+          <ReactionButtons post={post}/>
+          <Link
+            to={`/posts/${post.id}`}
+            className="button muted-button"
+          >View Post</Link>
+        </article>
+      );
+    })
+  } else if (postStatus === 'error') {
+    content = <div>{error}</div>
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   );
+
+  // const renderedPosts = orderedPosts.map(post => {
+  //   return (
+  //     <article className="post-excerpt" key={post.id}>
+  //       <h3>{post.title}</h3>
+  //       <PostAuthor userId={post.user}/>
+  //       <TimeAgo timestamp={post.date}/>
+  //       <p className="post-content">{post.content.substring(0, 100)}</p>
+  //       <ReactionButtons post={post}/>
+  //       <Link
+  //         to={`/posts/${post.id}`}
+  //         className="button muted-button"
+  //       >View Post</Link>
+  //     </article>
+  //   );
+  // });
+  //
+  // return (
+  //   <section className="posts-list">
+  //     <h2>Posts</h2>
+  //     {renderedPosts}
+  //   </section>
+  // );
 }
